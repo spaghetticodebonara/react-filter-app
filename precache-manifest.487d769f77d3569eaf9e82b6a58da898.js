9f8b38a7a9d9438da73b1491e4ffcaf158aac984
Mateusz Korczyński <korczynskimm@gmail.com>
2019-02-08 18:24:50 +0100

    Updates

---
+++ precache-manifest.487d769f77d3569eaf9e82b6a58da898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1765281583603614a69d",
+    "url": "/react-filter-app/static/js/runtime~main.17652815.js"
+  },
+  {
+    "revision": "34ce8104cf8776f5e790",
+    "url": "/react-filter-app/static/js/main.34ce8104.chunk.js"
+  },
+  {
+    "revision": "431f9faccf388346d8d2",
+    "url": "/react-filter-app/static/js/1.431f9fac.chunk.js"
+  },
+  {
+    "revision": "34ce8104cf8776f5e790",
+    "url": "/react-filter-app/static/css/main.527846a6.chunk.css"
+  },
+  {
+    "revision": "db4d5844c5ec0ed63fe34db0004e0a38",
+    "url": "/react-filter-app/index.html"
+  }
+];
